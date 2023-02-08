3c4d26528b7d2f132e534f1cffd70a1f4985d008
starmickey <micaelaestrellamorales@gmail.com>
2023-02-08 15:17:29 -0300

    Create sign out method and port validation

--- controller.js
+++ controller.js
@@ -10,19 +10,32 @@ const userAccounts = [];
 
 // Export functions
 
+function signOut(reqPort) {
+    const userAccount = userAccounts.find(({ port }) => port === reqPort);
+    const index = userAccounts.indexOf(userAccount);
+    if (index > -1) {
+        userAccounts.splice(index, 1);
+    }
+
+}
+
+exports.signOut = signOut;
+
+
 function signup(name, password, port) {
 
     return new Promise((resolve, reject) => {
 
         mongooseInterface.createUser(name, password).then(function onFullFillment(userDTO) {
+            signOut(port);
             const newUserAccount = new UserAccount(userDTO.id, userDTO.name, UserAccountStatus.loggedIn, port);
             userAccounts.push(newUserAccount);
             resolve(newUserAccount);
 
         }, function onRejection(error) {
             reject(error);
-        })    
-        
+        })
+
     })
 }
 
@@ -31,17 +44,19 @@ exports.signup = signup;
 
 
 function login(name, password, port) {
+    console.log(userAccounts);
 
     return new Promise((resolve, reject) => {
-        
+
         mongooseInterface.getUser(name, password).then(function onFullFillment(userDTO) {
+            signOut(port);
             const newUserAccount = new UserAccount(userDTO.id, userDTO.name, UserAccountStatus.loggedIn, port);
             userAccounts.push(newUserAccount);
             resolve(newUserAccount);
-            
+
         }, function onRejection(error) {
             reject(error);
-        });   
+        });
 
     })
 }
@@ -49,8 +64,12 @@ function login(name, password, port) {
 exports.login = login;
 
 
-function getUserAccount (reqPort) {
+function getUserAccount(reqPort) {
     return userAccounts.find(({ port }) => port === reqPort);
 }
 
 exports.getUserAccount = getUserAccount;
+
+
+
+
