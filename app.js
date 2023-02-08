826cf54f5fd806f2559bbfc45e7190100cfc47f1
starmickey <micaelaestrellamorales@gmail.com>
2023-02-08 12:53:28 -0300

    Create Log In and Sign Up web pages

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 
-app.get('/', function (req, res) {
-    res.render('index');
+app.get('/signup', function (req, res) {
+    res.render('signup');
+})
+
+p.get('/login', function (req, res) {
+    res.render('login');
 })
 
 app.listen(process.env.PORT || 3000, function () {
