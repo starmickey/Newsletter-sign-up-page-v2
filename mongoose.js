ba46de4b6e64dfe9301bfb05a9aba980e9a9ec59
starmickey <micaelaestrellamorales@gmail.com>
2023-02-08 11:35:38 -0300

    Create and get users from db

--- mongoose.js
+++ mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { UserDTO } = require(__dirname + '/data-objects/UserDTO.js');
 
 
 // Establish mongoose connection
@@ -12,3 +13,62 @@ async function main() {
 }
 
 
+// Schemas
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = new mongoose.model('user', userSchema);
+
+
+
+// Export functions
+
+function createUser(name, password) {
+    return new Promise((resolve, reject) => {
+        User.findOne({name: name}, function (error, user) {
+            if(error) {
+                reject(error);
+            } else if (user !== null) {
+                reject('user already created');
+            } else {
+                const user = new User({name: name, password: password});
+                user.save().then(function (user) {
+                    resolve(new UserDTO(user.id, user.name));
+                });
+                
+            }
+        })
+        
+    })
+}
+
+function getUser(name, password) {
+    return new Promise((resolve, reject) => {
+        User.findOne({name: name, password: password}, function (error, user) {
+            if (error) {
+                reject(error);
+            } else if (user === null) {
+                reject('user not found');
+
+            } else {
+                resolve(new UserDTO(user.id, user.name));
+            }
+        })
+    })
+}
+
+getUser('test5','test').then(function onFullfilled(user) {
+    console.log(user);
+}, function onRejection(error) {
+    console.log(error);
+})
